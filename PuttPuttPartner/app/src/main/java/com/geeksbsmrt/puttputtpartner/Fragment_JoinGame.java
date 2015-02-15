95e728dcea0f21c5e9ee502ae57bf4c3035723bd
Adam Crawford <geeksbsmrt@gmail.com>
2015-02-14 20:25:12 -0500

    Add Join Game temporary toast.  Linked Manual Check-in Unlisted button to Add Course activity.

--- PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/Fragment_JoinGame.java
+++ PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/Fragment_JoinGame.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Fragment_JoinGame extends Fragment {
 
@@ -25,10 +27,22 @@ public class Fragment_JoinGame extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_fragment__join_game, container, false);
+
         MainActivity.actionBar.setTitle(R.string.app_name);
         setHasOptionsMenu(true);
         MainActivity.actionBar.setHomeButtonEnabled(true);
         MainActivity.actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Button join = (Button) rootView.findViewById(R.id.JG_Join);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Remove toast message and complete Parse DB integration.
+                //Static set text in toast message as it will not be present in final build.
+                Toast.makeText(getActivity(), "Join functionality will be enabled in a future release.", Toast.LENGTH_LONG).show();
+            }
+        });
+
         return rootView;
     }
 
--- PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/Fragment_ManualCheckIn.java
+++ PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/Fragment_ManualCheckIn.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
 
 public class Fragment_ManualCheckIn extends Fragment {
 
@@ -24,10 +27,26 @@ public class Fragment_ManualCheckIn extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_fragment__manual__check_in, container, false);
+
         MainActivity.actionBar.setTitle(R.string.app_name);
         setHasOptionsMenu(true);
         MainActivity.actionBar.setHomeButtonEnabled(true);
         MainActivity.actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Button unlisted = (Button) rootView.findViewById(R.id.MCI_Unlisted);
+        unlisted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment_AddCourse ac = new Fragment_AddCourse();
+                getFragmentManager().beginTransaction().replace(R.id.container, ac).addToBackStack(null).commit();
+            }
+        });
+
+        ListView favList = (ListView) rootView.findViewById(R.id.MCI_FavCourses);
+        ListView nearList = (ListView) rootView.findViewById(R.id.MCI_NearCourses);
+
+
+
         return rootView;
     }
 
