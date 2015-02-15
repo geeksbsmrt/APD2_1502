6c7575c05a733a7bad9c31abe118eb16aef4bf9c
Adam Crawford <geeksbsmrt@gmail.com>
2015-02-14 20:46:57 -0500

    Added temporary toasts for AddCourse activity.

--- PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/Fragment_AddCourse.java
+++ PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/Fragment_AddCourse.java
@@ -7,9 +7,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Fragment_AddCourse extends Fragment {
+public class Fragment_AddCourse extends Fragment implements View.OnClickListener {
+
+    private LinearLayout playButtons;
 
     public Fragment_AddCourse() {
         // Required empty public constructor
@@ -32,6 +37,15 @@ public class Fragment_AddCourse extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         stateSpinner.setAdapter(adapter);
 
+        Button createScorecard = (Button) rootView.findViewById(R.id.AC_CreateScorecard);
+        createScorecard.setOnClickListener(this);
+        Button afp = (Button) rootView.findViewById(R.id.AC_AddFavAndPlay);
+        afp.setOnClickListener(this);
+        Button play = (Button) rootView.findViewById(R.id.AC_PlayOnce);
+        play.setOnClickListener(this);
+
+        playButtons = (LinearLayout) rootView.findViewById(R.id.AC_PlayButtons);
+
         return rootView;
     }
 
@@ -45,6 +59,28 @@ public class Fragment_AddCourse extends Fragment {
             default: {
                 return false;
             }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.AC_CreateScorecard:{
+                //TODO: Remove toast message and complete Parse DB integration.
+                //Static set text in toast message as it will not be present in final build.
+                Toast.makeText(getActivity(), "Scorecard functionality will be enabled in a future release.", Toast.LENGTH_LONG).show();
+                playButtons.setVisibility(View.VISIBLE);
+                break;
+            }
+            case R.id.AC_AddFavAndPlay:{
+                Toast.makeText(getActivity(), "Favorite and play functionality will be enabled in a future release.", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.AC_PlayOnce:{
+                Toast.makeText(getActivity(), "Play functionality will be enabled in a future release.", Toast.LENGTH_LONG).show();
+                break;
+            }
+            default: break;
         }
     }
 }
