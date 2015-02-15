465cd7609ea714b41b704fa19462e6a21d84d845
Adam Crawford <geeksbsmrt@gmail.com>
2015-02-14 21:30:57 -0500

    Setup options button to launch options menu.

--- PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/MainActivity.java
+++ PuttPuttPartner/app/src/main/java/com/geeksbsmrt/puttputtpartner/MainActivity.java
@@ -37,16 +37,18 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                Fragment_Options opts = new Fragment_Options();
+                getFragmentManager().beginTransaction().replace(R.id.container, opts).addToBackStack(null).commit();
+                return false;
+            }
+            default: {
+                return false;
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
 }
